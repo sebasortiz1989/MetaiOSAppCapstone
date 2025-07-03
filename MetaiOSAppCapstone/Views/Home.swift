660c90c8f8626857eb78a69acb3545f3bf16ea85
Sebastian <sebasortizsanta>
2025-07-02 21:40:17 -0300

    Sebas: [add] Exercise setup the navigation

---
+++ MetaiOSAppCapstone/Views/Home.swift
@@ -0,0 +1,24 @@
+//
+//  Home.swift
+//  MetaiOSAppCapstone
+//
+//  Created by Sebastian Ortiz on 02/07/25.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        TabView {
+            Menu()
+                .tabItem {
+                    Label("Menu", systemImage: "list.dash")
+                }
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+}
+
+#Preview {
+    Home()
+}
