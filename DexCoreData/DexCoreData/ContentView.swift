38b485aefe6eeede55b2fd629f3c9b43f476df8a
Sebastian <sebasortizsanta>
2025-07-05 20:56:12 -0300

    Sebas: [add] Started building FetchedPokemon model

--- DexCoreData/DexCoreData/ContentView.swift
+++ DexCoreData/DexCoreData/ContentView.swift
@@ -25,7 +25,7 @@ struct ContentView: View {
                     } label: {
                         Text(pokemon.name ?? "no name")
                     }
-                }ç
+                }
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
