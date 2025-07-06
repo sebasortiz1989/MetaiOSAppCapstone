c5b303d62c8105cf6535b3a46b74851223a1b3fc
Sebastian <sebasortizsanta>
2025-07-06 13:58:48 -0300

    Sebas: [add] Added searchbar

--- DexCoreData/DexCoreData/ContentView.swift
+++ DexCoreData/DexCoreData/ContentView.swift
@@ -10,43 +10,72 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Pokemon.id, ascending: true)],
-        animation: .default)
-
-    private var pokedex: FetchedResults<Pokemon>
-
+    
+    @FetchRequest<Pokemon>(
+        sortDescriptors: [SortDescriptor(\.id)],
+        animation: .default,
+    ) private var pokedex
+    
+    @State private var searchText: String = ""
+    
     let fetcher = FetchService()
+    
+    private var dynamicPredicate: NSPredicate {
+        var predicates: [NSPredicate] = []
+        
+        // Search predicate
+        if (!searchText.isEmpty) {
+            predicates.append(NSPredicate(format: "name contains[c] %@", searchText))
+        }
+        
+        // Filter by favorite predicate
+        
+        // Combine predicates
+        
+        return NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
+    }
     
     var body: some View {
         NavigationStack {
             List {
                 ForEach(pokedex) { pokemon in
                     NavigationLink(value: pokemon) {
-                        AsyncImage(url: pokemon.sprite) { image in
-                            image
-                                .resizable()
-                                .scaledToFit()
-                        } placeholder: {
-                            ProgressView()
-                        }
-                        .frame(width: 100, height: 100)
-                        
-                        VStack(alignment: .leading) {
-                            Text(pokemon.name!.capitalized)
-                                .fontWeight(.bold)
+                        HStack {
+                            AsyncImage(url: pokemon.sprite) { image in
+                                image
+                                    .resizable()
+                                    .scaledToFit()
+                            } placeholder: {
+                                ProgressView()
+                            }
+                            .frame(width: 100, height: 100)
                             
-                            HStack {
-                                ForEach(pokemon.types!, id: \.self) { type in
-                                    Text(type.capitalized)
-                                        .font(.subheadline)
-                                        .fontWeight(.semibold)
-                                        .foregroundStyle(.black)
-                                        .padding(.horizontal, 13)
-                                        .padding(.vertical, 5)
-                                        .background(Color(type.capitalized))
-                                        .clipShape(.capsule)
+                            VStack(alignment: .leading) {
+                                HStack {
+                                    Circle()
+                                        .fill(Color.accentColor)
+                                        .frame(width: 30, height: 30)
+                                        .overlay(
+                                            Text("\(pokemon.id)")
+                                                .foregroundStyle(.white)
+                                                .font(.system(size: 12))
+                                                .fontWeight(.bold)
+                                        )
+                                    Text(pokemon.name!.capitalized)
+                                        .fontWeight(.bold)
+                                }
+                                
+                                HStack {
+                                    ForEach(pokemon.types!, id: \.self) { type in
+                                        Text(type.capitalized)
+                                            .font(.subheadline)
+                                            .fontWeight(.semibold)
+                                            .foregroundStyle(.black)
+                                            .padding(.horizontal, 13)
+                                            .padding(.vertical, 5)
+                                            .background(Color(type.capitalized))
+                                            .clipShape(.capsule)
+                                    }
                                 }
                             }
                         }
@@ -54,6 +83,11 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Pokedex")
+            .searchable(text: $searchText, prompt: "Find a Pokemon")
+            .autocorrectionDisabled()
+            .onChange(of: searchText) {
+                pokedex.nsPredicate = dynamicPredicate
+            }
             .navigationDestination(for: Pokemon.self) { pokemon in
                 Text(pokemon.name ?? "no name")
             }
