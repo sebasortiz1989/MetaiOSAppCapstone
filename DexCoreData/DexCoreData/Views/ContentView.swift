a82ed7b787fa5f4b3d5ba98f9e28bd835c61bef4
Sebastian <sebasortizsanta>
2025-07-11 11:39:07 -0300

    Sebas: [add] Show stored images

--- DexCoreData/DexCoreData/Views/ContentView.swift
+++ DexCoreData/DexCoreData/Views/ContentView.swift
@@ -65,14 +65,22 @@ struct ContentView: View {
                         ForEach(pokedex) { pokemon in
                             NavigationLink(value: pokemon) {
                                 HStack {
-                                    AsyncImage(url: pokemon.spriteURL) { image in
-                                        image
+                                    if (pokemon.sprite == nil) {
+                                        AsyncImage(url: pokemon.spriteURL) { image in
+                                            image
+                                                .resizable()
+                                                .scaledToFit()
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
+                                        .frame(width: 100, height: 100)
+                                    } else {
+                                        pokemon.spriteImage
                                             .resizable()
                                             .scaledToFit()
-                                    } placeholder: {
-                                        ProgressView()
+                                            .frame(width: 100, height: 100)
                                     }
-                                    .frame(width: 100, height: 100)
+            
                                     
                                     VStack(alignment: .leading) {
                                         HStack {
