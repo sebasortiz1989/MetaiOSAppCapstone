a188befbf826cc8f76cd7b45b76f5dd80f07ab97
Sebastian <sebasortizsanta>
2025-08-12 22:08:48 -0300

    Sebas: [add] Use Model Container for Persistence

--- DexCoreData/DexCoreData/Persistence.swift
+++ DexCoreData/DexCoreData/Persistence.swift
@@ -5,69 +5,26 @@
 //  Created by Sebastian Ortiz on 04/07/25.
 //
 
-import CoreData
+import SwiftData
+import Foundation
 
+@MainActor
 struct PersistenceController {
-    // The thing that controls our database
-    static let shared = PersistenceController()
-    
     static var previewPokemon: Pokemon {
-        let context = PersistenceController.preview.container.viewContext
-        
-        let fetchRequest: NSFetchRequest<Pokemon> = Pokemon.fetchRequest()
-        fetchRequest.fetchLimit = 1
-        
-        let results = try! context.fetch(fetchRequest)
-        
-        return results.first!
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+
+        let pokemonData = try! Data(contentsOf: Bundle.main.url(forResource: "samplePokemon", withExtension: "json")!)
+        let pokemon = try! decoder.decode(Pokemon.self, from: pokemonData)
+        return pokemon;
     }
 
-    // The thing that controls our sample preview database
-    static let preview: PersistenceController = {
-        let result = PersistenceController(inMemory: true)
-        let viewContext = result.container.viewContext
+    // Our sample preview database
+    static let preview: ModelContainer = {
+        let container = try! ModelContainer(for: Pokemon.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
         
-        let newPokemon = Pokemon(context: viewContext)
-        newPokemon.id = 1
-        newPokemon.name = "bulbasaur"
-        newPokemon.types = ["grass", "poison"]
-        newPokemon.hp = 45
-        newPokemon.attack = 49
-        newPokemon.defense = 49
-        newPokemon.specialAttack = 65
-        newPokemon.specialDefense = 65
-        newPokemon.speed = 45
-        newPokemon.spriteURL = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png")
-        newPokemon.shinyURL = URL(string: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/1.png")
+        container.mainContext.insert(previewPokemon)
         
-        do {
-            try viewContext.save()
-        } catch {
-            print(error)
-        }
-        return result
+        return container
     }()
-
-    // The thing that holds the stuff (The database)
-    let container: NSPersistentContainer
-
-    // Just a regular init function
-    init(inMemory: Bool = false) {
-        container = NSPersistentContainer(name: "DexCoreData")
-        if inMemory {
-            container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
-        } else {
-            container.persistentStoreDescriptions.first!.url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.sebapps.DexCoreGroup")!.appending(path: "DexCoreData.sqlite")
-        }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                print(error)
-            }
-        })
-        
-        // Keep the one in the database and forget about the new one added
-        container.viewContext.mergePolicy = NSMergePolicy.mergeByPropertyStoreTrump
-
-        container.viewContext.automaticallyMergesChangesFromParent = true
-    }
 }
