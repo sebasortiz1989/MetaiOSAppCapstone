efbd8807f2b1d13ca7ab3b940dba6b51fd4af85a
Sebastian <sebasortizsanta>
2025-08-14 22:32:59 -0300

    Sebas: [add] Fixed Widget

--- DexCoreData/DexWidget/DexWidgetBundle.swift
+++ DexCoreData/DexWidget/DexWidgetBundle.swift
@@ -1,16 +1,16 @@
-////
-////  DexWidgetBundle.swift
-////  DexWidget
-////
-////  Created by Sebastian Ortiz on 12/07/25.
-////
 //
-//import WidgetKit
-//import SwiftUI
+//  DexWidgetBundle.swift
+//  DexWidget
 //
-//@main
-//struct DexWidgetBundle: WidgetBundle {
-//    var body: some Widget {
-//        DexWidget()
-//    }
-//}
+//  Created by Sebastian Ortiz on 12/07/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DexWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DexWidget()
+    }
+}
